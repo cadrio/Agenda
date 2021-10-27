d0ad0ee523b0a8b835b5a0858972bdb4749fa466
cadrio <crmjrx@gmail.com>
2021-10-27 13:09:00 -0300

    Alterado número de telefone padrão e espaçamento de linha.

--- app/src/main/java/com/example/agenda/AgendaApplication.java
+++ app/src/main/java/com/example/agenda/AgendaApplication.java
@@ -16,6 +16,6 @@ public class AgendaApplication extends Application {
     }
     private void criaAlunosTeste() {
         AlunoDAO dao = new AlunoDAO();
-        dao.salvar(new Aluno("Carlos", "crmjrx@gmail.com", "997474427"));
+        dao.salvar(new Aluno("Carlos", "crmjrx@gmail.com", "997474527"));
     }
 }
